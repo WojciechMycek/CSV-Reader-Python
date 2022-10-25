70123c526bf44700e2ebb2ac2b8b64d2d70dd4d2
Wojciech Mycek <wojciech.mycek@nokia.com>
2022-10-25 20:37:04 +0200

    class-method-read-from-csv

--- CSV-reader/CSV-reader/CSV_reader.py
+++ CSV-reader/CSV-reader/CSV_reader.py
@@ -1,1 +1,16 @@
-print("Witaj")
+from asyncio.windows_events import NULL
+import csv
+
+class MaintainCsvFile():
+   
+    classmethod
+    def read_from_csv(csv_file_name):
+        with open(csv_file_name,'r') as r:
+            reader = csv.DictReader(r)
+            items = list(reader)
+
+        for item in items:
+            print(item)
+
+
+MaintainCsvFile.read_from_csv("Notatnik")
